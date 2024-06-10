e4968bd90cd47b9a6283d1a843b8fbc832e53b11
serla vamshi krishna <77665988+vamshi99145@users.noreply.github.com>
2024-06-10 09:11:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,241 @@
+
+
+adWebsites = [
+    "*://*.doubleclick.net/*",
+    "*://.partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.creative.ak.fbcdn.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.admob.com/*",
+    "*://*.adnxs.com/*",
+    "*://*.ads.yahoo.com/*",
+    "*://*.advertising.com/*",
+    "*://*.adsterra.com/*",
+    "*://*.adf.ly/*",
+    "*://*.propellerads.com/*",
+    "*://*.popads.net/*",
+    "*://*.revcontent.com/*",
+    "*://*.outbrain.com/*",
+    "*://*.taboola.com/*",
+    "*://*.mgid.com/*",
+    "*://*.exoclick.com/*",
+    "*://*.trafficfactory.biz/*",
+    "*://*.trafficjunky.net/*",
+    "*://*.adxpansion.com/*",
+    "*://*.adsco.re/*",
+    "*://*.adnium.com/*",
+    "*://*.adshost2.com/*",
+    "*://*.cams4free.com/*",
+    "*://*.hdpornvideos.com/*",
+    "*://*.shemale.com/*",
+    "*://*.pornprosnetwork.com/*",
+    "*://*.gayprofit.com/*",
+    "*://*.pornstarnetwork.com/*",
+    "*://*.sextronix.com/*",
+    "*://*.twistysnetwork.com/*",
+    "*://*.playboyplus.com/*",
+    "*://*.adult-empire.com/*",
+    "*://*.pornaccess.com/*",
+    "*://*.onlyfans.com/*",
+    "*://*.playboy.tv/*",
+    "*://*.sex.com/*",
+    // Add more ad websites if needed
+];
+
+// List of 18+ websites to block
+adultWebsites = [
+    "*://*.pornhub.com/*",
+    "*://*.xvideos.com/*",
+    "*://*.xnxx.com/*",
+    "*://*.redtube.com/*",
+    "*://*.youporn.com/*",
+    "*://*.tube8.com/*",
+    "*://*.chaturbate.com/*",
+    "*://*.livejasmin.com/*",
+    "*://*.bongacams.com/*",
+    "*://*.camsoda.com/*",
+    "*://*.spankbang.com/*",
+    "*://*.youjizz.com/*",
+    "*://*.hclips.com/*",
+    "*://*.bravotube.net/*",
+    "*://*.tnaflix.com/*",
+    "*://*.alohatube.com/*",
+    "*://*.hclips.com/*",
+    "*://*.drtuber.com/*",
+    "*://*.xhamster.com/*",
+    "*://*.mofos.com/*",
+    "*://*.realitykings.com/*",
+    "*://*.bangbros.com/*",
+    "*://*.sexvid.pro/*",
+    "*://*.pornpapa.com/*",
+    "*://*.hot-sex-tube/*",
+    "*://*.okxxx2.com/*",
+    "*://*.xnxx.health.com/*",
+    "*://*.inxxx.com/*",
+    "*://*.iporntv.net/*",
+
+    "*://*.xnxxcom.xyz/*",
+    "*://*.rajwap.cc/*",
+    "*://*.porno18.site/*",
+    "*://*.xnxx.com.se/*",
+    "*://*.xxxvideo.link/*",
+
+
+    // Add more 18+ websites if needed
+];
+
+// List of betting websites to block
+bettingWebsites = [
+    "*://*.bet365.com/*",
+    "*://*.betfair.com/*",
+    "*://*.williamhill.com/*",
+    "*://*.paddypower.com/*",
+    "*://*.betway.com/*",
+    "*://*.888sport.com/*",
+    "*://*.ladbrokes.com/*",
+    "*://*.unibet.com/*",
+    "*://*.coral.co.uk/*",
+    "*://*.bovada.lv/*",
+    "*://*.draftkings.com/*",
+    "*://*.fanduel.com/*",
+    "*://*.betonline.ag/*",
+    "*://*.mybookie.ag/*",
+    "*://*.bovada.lv/*",
+    "*://*.sportsbet.io/*",
+    "*://*.pokerstars.com/*",
+    "*://*.casinowinner.com/*",
+    "*://*.10bet.com/*",
+    "*://*.4rabet-india.in/*",
+    "*://*.lotus365.com/*",
+    "*://*.mahdevapp.com/*",
+    "*://*.bet365india.in/*",
+    "*://*.bettingsites.org/*",
+    "*://*.betsson.com/*",
+    "*://*.bet-at-home.com/*",
+    "*://*.betvictor.com/*",
+    "*://*.sbobet.com/*",
+    "*://*.32red.com/*",
+    "*://*.888casino.com/*",
+    "*://*.royalpanda.com/*",
+    "*://*.betfred.com/*",
+    "*://*.mrgreen.com/*",
+    "*://*.betdaq.com/*",
+    "*://*.intertops.eu/*",
+    "*://*.matchbook.com/*",
+    "*://*.betstars.com/*",
+    "*://*.sportingbet.com/*",
+    "*://*.sportsinteraction.com/*",
+    "*://*.winner.com/*",
+    "*://*.expekt.com/*",
+    "*://*.nordicbet.com/*",
+    "*://*.redbet.com/*",
+    "*://*.superbahis.com/*",
+    "*://*.vip.bsportin.com/*",
+    "*://*.lotusss365.in/*",
+
+];
+var pornQuotes = [
+    "Pornography & Betting apps is not just a fantasy, it's a reality that distorts our perception of intimacy and relationships. - Brittni De La Mora",
+    "Pornography  & Betting appsis a silent destroyer of love, eroding the foundations of trust and intimacy. - Dr. Donald L. Hilton Jr.",
+    "Pornography  & Betting apps promises pleasure but delivers emptiness, leaving us disconnected from ourselves and others.  - Dr. Norman Doidge" ,
+    "The illusion of pleasure in pornography masks the reality of its harm to our minds, hearts, and relationships. - Dr. Gail Dines",
+    "Pornography  & Betting apps offers a counterfeit version of intimacy, robbing us of the genuine connections that fulfill our souls. - Jessica Harris",
+    "Pornography  & Betting apps hijacks our brains, replacing real intimacy with artificial stimulation. - Matthew Fradd",
+    "Pornography & Betting apps fuels a cycle of dissatisfaction, driving us to seek more while leaving us empty-handed. - Dr. Kevin Skinner",
+    "Pornography & Betting apps may promise liberation, but it enslaves us to our basest desires, trapping us in a cycle of addiction. - Mark Kastleman",
+    "Pornography  & Betting appsoffers a distorted view of sex, leaving us disillusioned and disconnected from the beauty of true intimacy. - Dr. Lisa M. Diamond",
+    "Pornography & Betting apps is a toxic substitute for real connection, poisoning our minds and hearts with its empty promises. - Joshua Shea"
+ ];
+
+
+ var bettingQuotes = [
+    "Pornography & Betting apps is not just a fantasy, it's a reality that distorts our perception of intimacy and relationships. - Brittni De La Mora",
+    "Pornography  & Betting appsis a silent destroyer of love, eroding the foundations of trust and intimacy. - Dr. Donald L. Hilton Jr.",
+    "Pornography  & Betting apps promises pleasure but delivers emptiness, leaving us disconnected from ourselves and others.  - Dr. Norman Doidge" ,
+    "The illusion of pleasure in pornography masks the reality of its harm to our minds, hearts, and relationships. - Dr. Gail Dines",
+
+    "Betting apps & Porn may offer a rush of excitement, but they can also lead to a lifetime of regret. - David Lee",
+
+"The thrill of victory in betting apps & Porn often comes with the agony of defeat, leaving us chasing an elusive high. - Benjamin Franklin",
+"Pornography  & Betting apps offers a counterfeit version of intimacy, robbing us of the genuine connections that fulfill our souls. - Jessica Harris",
+"Pornography  & Betting apps hijacks our brains, replacing real intimacy with artificial stimulation. - Matthew Fradd",
+"Pornography & Betting apps fuels a cycle of dissatisfaction, driving us to seek more while leaving us empty-handed. - Dr. Kevin Skinner",
+
+"Betting apps & Porn promise quick riches, but they often lead to financial ruin and shattered dreams. - John Stuart Mill",
+
+"The allure of betting apps & Porn is a siren song, leading many astray into a sea of financial instability. - Warren Buffett",
+
+"Betting apps & Porn are like a mirage in the desert, promising riches but leaving us thirstier than ever. - Aristotle",
+"Pornography & Betting apps may promise liberation, but it enslaves us to our basest desires, trapping us in a cycle of addiction. - Mark Kastleman",
+"Pornography  & Betting appsoffers a distorted view of sex, leaving us disillusioned and disconnected from the beauty of true intimacy. - Dr. Lisa M. Diamond",
+
+"The odds may seem in your favor with betting apps & Porn, but in the end, the house always wins. - Napoleon Bonaparte",
+
+"Betting apps & Porn offer a fleeting thrill at the expense of long-term financial security. - Warren Buffet",
+"Pornography & Betting apps is a toxic substitute for real connection, poisoning our minds and hearts with its empty promises. - Joshua Shea",
+"Betting apps  & Pornmay offer a temporary escape, but they ultimately lead to a prison of debt and despair. - George Soros",
+
+"Betting apps & Porn are like a rigged game of chance, designed to enrich the few at the expense of the many. - Benjamin Graham",
+
+"Betting apps & Porn may promise excitement, but they often deliver heartache and regret. - Carl Icahn"
+
+ ];
+
+
+
+function getRandomQuote(quotes) {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+
+function displayRandomQuote() {
+    console.log("Inside displayRandomQuote function...");
+    var currentUrl = window.location.href;
+    console.log("Current URL:", currentUrl);
+    var quoteToShow;
+
+
+    var isAdultWebsite = false;
+    for (var i = 0; i < adultWebsites.length; i++) {
+        if (currentUrl.includes(adultWebsites[i])) {
+            console.log(currentUrl.includes(adultWebsites[i]));
+            isAdultWebsite = true;
+            break;
+        }
+    }
+    console.log("isAdultWebsite:", isAdultWebsite);
+
+    if (isAdultWebsite) {
+        quoteToShow = getRandomQuote(pornQuotes);
+    } else {
+        quoteToShow = getRandomQuote(bettingQuotes);
+    }
+
+    console.log("Quote to show:", quoteToShow);
+    document.getElementById("quote").innerText = quoteToShow;
+}
+
+window.onload = function() {
+    displayRandomQuote();
+
+   
+};
+
+
+var allBlockedWebsites = adWebsites.concat(adultWebsites, bettingWebsites);
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        console.log("Inside onBeforeRequest function...");
+        return {
+            redirectUrl: chrome.extension.getURL("blocked.html")
+        };
+    },
+    { urls: allBlockedWebsites },
+    ["blocking"]
+);
